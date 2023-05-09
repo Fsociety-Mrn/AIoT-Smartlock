11210cdf49c337e191421c1977bdf7673fec58dd
Fsociety-Mrn <lisboamillen30@gmail.com>
2023-05-10 03:26:52 +0800

    hindi pa tapos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(11, 131, 120)',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+    info: {
+      main: '#F7C873',
+    },
+    text:{
+      disabled: '#000000',
+    },
+    error:{
+      main: '#c71e1e',
+    },
+    warning:{
+      main: '#ff8c00'
+    },
+    success:{
+      main: '#32cd32'
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
-
+// background: 'linear-gradient(to right, rgb(11, 131, 120) 0%, rgb(85, 98, 112) 100%)'
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,149 @@
-import { Box, Grid  } from '@mui/material'
+import { Avatar, Box, Button, Divider, Grid, Link, Paper, Stack, TextField, Typography  } from '@mui/material'
 import React from 'react'
 
 const Login = () => {
+
+  // get windows screen
+const [state, setState] = React.useState(true);
+React.useEffect(()=>{
+    const setResponsiveness = () => {
+        return window.innerWidth < 700 ? setState(true) : setState(false);
+    };
+
+    setResponsiveness();
+        window.addEventListener("resize", () => setResponsiveness());
+    return () => {
+        window.removeEventListener("resize", () => setResponsiveness());
+    };
+},[])
+
   return (
-    <div>
+    <>
+      {state ? <Mobile />  : <Desktop />} 
+    </>
+  )
+}
+
+// Mobile view
+
+
+const Mobile = () => {
+  return (
+    <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+    }}>
+      <Grid 
+      container   
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{ 
+        minHeight: "100vh"
+      }}
+      >
+
+        <Grid item xs={8}>
+
+
+        <Box
+          sx={{
+            width: '90vw',
+            height: '80vh',
+            backgroundColor: 'primary.dark',
+            borderTopLeftRadius: '50px',
+            background: 'linear-gradient(to right, rgb(11, 131, 120) 0%, rgb(85, 98, 112) 100%)',
+            // backgroundSize: '200% 200%',
+            // backgroundPosition: 'left',
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center'
+          }}
+          >
+
+            <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            >
+
+            
+              <Avatar
+              sizes='40px'
+              sx={{ 
+                bgcolor: "#e7e5d6b5",
+                height: '150px', width: '150px'
+              }}>S</Avatar>
+
+<Grid container       
+  direction="row"
+      justifyContent="center"
+      alignItems="center" padding={2}>
+
+          
+                <TextField type='email' margin='dense' label="Email" variant="outlined" fullWidth  size='medium'/>
+                <TextField type='password' margin='dense' label="Password" variant="outlined" fullWidth  size='medium'/>
+                <Link href="#" underline="always" style={{
+                    color: 'white'
+                  }} >Forgot Password</Link>
+                  <br/>
+
+            <Button variant='contained' fullWidth 
+                  style={{
+                    background: 'linear-gradient(to right, rgb(11, 131, 120) 0%, rgb(85, 98, 112) 100%)'
+                  }}>Login</Button>
+
+                <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                >
+           
+
+                </Stack>
+
+                <Grid item xs={12}>
+                <Divider><Typography
+                style={{
+                  color: '#FFFFFF'
+                }}>Or</Typography></Divider>
+                </Grid>
+
+                <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                >
+                  <Button variant='contained' fullWidth
+                  style={{
+                    color: 'rgb(11, 131, 120)',
+                    backgroundColor: "white"
+                  }}
+                  >Login with Google</Button>
+                </Stack>
+                </Grid>
+
+
+
+                </Stack>
+
+
+
+          </Box>
+        </Grid>
+
+      </Grid>
+    </div>
+  )
+}
+
+// Desktop view
+const Desktop = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
     
       <Grid 
       container   
@@ -12,36 +152,110 @@ const Login = () => {
       alignItems="center"
       spacing={0}
       style={{ 
-        minHeight: "90vh"
+        minHeight: "100vh"
       }}
       >
-
-      {/* lineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0.477, stop:0 rgba(11, 131, 120, 219), stop:1 rgba(85, 98, 112, 226));\n"
-                                  "    color:rgba(255, 255, 255, 210) */}
         <Grid item>
-          
           <Box
             sx={{
-              width: 280,
-              height: 430,
+              
+              width: 380,
+              height: 530,
               backgroundColor: 'primary.dark',
               borderTopLeftRadius: '50px',
-              background: 'linear-gradient(to right, rgba(11, 131, 120, 0.86) 0%, rgba(85, 98, 112, 0.89) 100%)' 
-
+              background: 'linear-gradient(to right, rgb(11, 131, 120) 0%, rgb(85, 98, 112) 100%)',
+              backgroundSize: '200% 200%',
+              backgroundPosition: 'left',
+              display: 'flex', 
+              justifyContent: 'center', 
+              alignItems: 'center'
             }}
-          />
+          >
+
+            <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            marginBottom={10}
+            >
+            <Avatar
+            sizes='40px'
+             sx={{ 
+              bgcolor: "#e7e5d6b5",
+              height: '280px', width: '280px'
+            }}>S</Avatar>
+            </Stack>
+          
+          </Box>
 
         </Grid>
 
         <Grid item>
           <Box
-            sx={{
-              width: 240,
-              height: 430,
-              backgroundColor: 'white',
-              borderBottomRightRadius: '50px'
-            }}
-          />
+          sx={{
+            width: 340,
+            height: 530,
+            backgroundColor: 'white',
+            borderBottomRightRadius: '50px',
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+          }}
+          >
+            <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            marginX={2}
+            marginTop={5}
+            >
+            
+              <Paper
+              elevation={0}
+              >
+                <TextField type='email' margin='dense' label="Email" variant="outlined" fullWidth  size='medium'/>
+                <TextField type='password' margin='dense' label="Password" variant="outlined" fullWidth  size='medium'/>
+                
+                <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                padding={3}
+                spacing={1}
+                >
+                  <Link href="#" style={{
+                    color: 'rgb(11, 131, 120)'
+                  }} >Forgot Password</Link>
+                  <Button variant='contained' fullWidth style={{
+                    background: 'linear-gradient(to right, rgb(11, 131, 120) 0%, rgb(85, 98, 112) 100%)'
+                  }}>Login</Button>
+
+                </Stack>
+
+                <Divider><Typography
+                style={{
+                  color: 'rgb(11, 131, 120)'
+                }}>Or</Typography></Divider>
+
+                <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                padding={3}
+                spacing={1}
+                >
+                  <Button variant='contained' fullWidth
+                  style={{
+                    color: 'rgb(11, 131, 120)',
+                    border: 'solid 2px rgb(11, 131, 120)',
+                    backgroundColor: "white"
+                  }}
+                  >Login with Google</Button>
+                </Stack>
+
+              </Paper>
+
+            </Stack>
+
+          </Box>
         </Grid>
 
       </Grid>
@@ -49,6 +263,8 @@ const Login = () => {
     </div>
   )
 }
+
+
 
 export default Login
 
