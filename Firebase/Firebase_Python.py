4b7acef4330b8c06606604a9bfa41a1169531428
unknown <lisboamillen30@gmail.com>
2023-10-03 10:55:38 +0800

    updated

--- Firebase/Firebase_Python.py
+++ Firebase/Firebase_Python.py
@@ -3,9 +3,10 @@ from firebase_admin import credentials,db
 
 cred = credentials.Certificate("Firebase/secret.json")
 default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://aiot-smartlock-default-rtdb.asia-southeast1.firebasedatabase.app/?offline=True"
+    'databaseURL': "https://aiot-smartlock-default-rtdb.asia-southeast1.firebasedatabase.app/"
 })
 
 ref = db.reference("/")
 
 print(ref.get())
+
