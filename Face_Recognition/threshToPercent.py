c24e1fd096f18bc3ca3a667b3fadfe85c8a4d88a
unknown <lisboamillen30@gmail.com>
2023-10-23 12:08:09 +0800

    updated

--- Face_Recognition/threshToPercent.py
+++ Face_Recognition/threshToPercent.py
@@ -11,7 +11,7 @@
 
 import math
 
-def face_distance_to_conf(face_distance, face_match_threshold=0.5):
+def face_distance_to_conf(face_distance, face_match_threshold=0.6):
     if face_distance > face_match_threshold:
         range = (1.0 - face_match_threshold)
         linear_val = (1.0 - face_distance) / (range * 2.0)
@@ -24,7 +24,20 @@ def face_distance_to_conf(face_distance, face_match_threshold=0.5):
         # range = face_match_threshold
         # linear_val = 1.0 - (face_distance / (range * 2.0))
         # return linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
+def printLoop(threshhold):
+    result = face_distance_to_conf(face_distance=threshhold)
+    result = int(result * 100)
 
-result = face_distance_to_conf(face_distance=0.0)
-result = int(result * 100)
-print(f"Percentage value: {result}%")
+    print(f"Percentage value: {result}%")
+
+loop = [
+    0.59,
+    0.55,
+    0.52,
+    0.54,
+    0.55,
+    0.00
+]
+
+for each in loop:
+    printLoop(each)
