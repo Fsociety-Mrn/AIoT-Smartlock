54902caf8225f93d33ebdfda7e9e49a64b3ed170
unknown <lisboamillen30@gmail.com>
2023-11-07 17:38:02 +0800

    dasdasd

--- Raspberry/Raspberry.py
+++ Raspberry/Raspberry.py
@@ -6,8 +6,16 @@ from Firebase.firebase import firebaseRead
 # GPIO.setwarnings(False)
 
 # # SETUP PIN
-# GPIO.setup(2,GPIO.OUT)
-# GPIO.setup(3,GPIO.OUT)
+# GPIO.setup(21,GPIO.OUT)
+# GPIO.setup(20,GPIO.OUT)
+# GPIO.setup(16,GPIO.OUT)
+# GPIO.setup(12,GPIO.OUT)
+# GPIO.setup(7,GPIO.OUT)
+# GPIO.setup(8,GPIO.OUT)
+# GPIO.setup(25,GPIO.OUT)
+# GPIO.setup(24,GPIO.OUT)
+# GPIO.setup(23,GPIO.OUT)
+# GPIO.setup(18,GPIO.OUT)
 
 def openLocker():
     data = firebaseRead("LOCK")
@@ -19,3 +27,5 @@ def OpenLockers(key,value):
     if value:
         print("Open Lockers")
         print(key,value)
+        
+# GPIO.cleanup()
