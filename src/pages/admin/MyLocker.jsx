1d9514dd95f3ffeb2d58d3415547ba2d6d5f5d39
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-11-10 13:05:50 +0800

    Update MyLocker.jsx

--- src/pages/admin/MyLocker.jsx
+++ src/pages/admin/MyLocker.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
+import React from 'react';
 
-function MyLocker() {
+const MyLocker = () => {
+  const cardStyle = {
+    width: '300px',
+    padding: '20px',
+    backgroundColor: '#f9f9f9',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    textAlign: 'center',
+  };
+
+  const buttonStyle = {
+    margin: '10px',
+    padding: '10px 20px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '100vh',
+  };
+
+  const buttonContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: '20px',
+  };
+
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <br/>
-    <br/>
-    <br/>
-
-    <br/>
-
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    
-      <h1>My locker</h1>
+    <div style={containerStyle}>
+      <div style={cardStyle}>
+        <h1 style={{ marginBottom: '20px' }}>My Locker</h1>
+        <div style={buttonContainerStyle}>
+          <button
+            style={buttonStyle}
+            onMouseOver={(e) => (e.target.style.backgroundColor = '#38597a')}
+            onMouseOut={(e) => (e.target.style.backgroundColor = '#45a8a8')}
+          >
+            Button 1
+          </button>
+          <button
+            style={buttonStyle}
+            onMouseOver={(e) => (e.target.style.backgroundColor = '#38597a')}
+            onMouseOut={(e) => (e.target.style.backgroundColor = '#45a8a8')}
+          >
+            Button 2
+          </button>
+          <button
+            style={buttonStyle}
+            onMouseOver={(e) => (e.target.style.backgroundColor = '#38597a')}
+            onMouseOut={(e) => (e.target.style.backgroundColor = '#45a8a8')}
+          >
+            Button 3
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyLocker
+export default MyLocker;
