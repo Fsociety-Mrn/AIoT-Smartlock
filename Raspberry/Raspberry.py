c65ee8219b1443a80373727720a0abdb3153a1fb
unknown <ogm.wd.intern4@gmail.com>
2023-11-22 16:44:46 +0800

    ADSASD

--- Raspberry/Raspberry.py
+++ Raspberry/Raspberry.py
@@ -1,22 +1,22 @@
 from Firebase.firebase import firebaseRead, lockerUpdate
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import threading
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-# # SETUP PIN
-# GPIO.setup(21,GPIO.OUT)
-# GPIO.setup(20,GPIO.OUT)
-# GPIO.setup(16,GPIO.OUT)
-# GPIO.setup(12,GPIO.OUT)
-# GPIO.setup(7,GPIO.OUT)
-# GPIO.setup(8,GPIO.OUT)
-# GPIO.setup(25,GPIO.OUT)
-# GPIO.setup(24,GPIO.OUT)
-# GPIO.setup(23,GPIO.OUT)
-# GPIO.setup(18,GPIO.OUT)
+# SETUP PIN
+GPIO.setup(21,GPIO.OUT)
+GPIO.setup(20,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(8,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
 def openLocker():
     try:
@@ -36,9 +36,9 @@ def OpenLockers(name,key,value):
         print("Open Lockers")
         print(key,value)
         
-        # GPIO.output(key,value)
+        GPIO.output(key,value)
         time.sleep(2)
-        # GPIO.output(key,GPIO.LOW)
+        GPIO.output(key,GPIO.LOW)
         
         lockerUpdate(name=name, value=False)
         
